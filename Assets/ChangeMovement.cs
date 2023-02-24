f006d366ab9f05dfa17e2f33da15624c0b6d7fbc
HumanError <96177058+NaphatMaunsiri@users.noreply.github.com>
2023-02-24 12:03:21 +0700

    Unity

---
+++ Assets/ChangeMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void getAnimate(){
+        animator.Play("Thriller Part 3");
+    }
+}
